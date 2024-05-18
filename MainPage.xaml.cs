3aef48b71a64225bb0cca5b6c985ee2cfc5067dc
VMekh8 <mekhvitaliy88@gmail.com>
2024-05-18 17:20:52 +0300

    Create Nav Buttons

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,5 +1,9 @@
 ﻿
+using Windows.UI;
+using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
 
 namespace FarmLandAccountingUWP
 {
@@ -13,6 +17,18 @@ namespace FarmLandAccountingUWP
             this.InitializeComponent();
         }
 
-  
+        private void NavButton_Hover (object sender, PointerRoutedEventArgs e)
+        {
+            RelativePanel panel = sender as RelativePanel;
+
+            panel.Background = new SolidColorBrush(Colors.DarkViolet);
+        }
+
+        private void NavButton_Cancel (object sender, PointerRoutedEventArgs e)
+        {
+            RelativePanel panel = sender as RelativePanel;
+
+            panel.Background = new SolidColorBrush(Colors.Transparent); 
+        }
     }
 }
