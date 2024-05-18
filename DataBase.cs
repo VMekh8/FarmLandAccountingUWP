c2f50992e0bb73cb990b0b046d94bc2291f3a3b3
VMekh8 <mekhvitaliy88@gmail.com>
2024-05-18 15:01:22 +0300

    Add database class

---
+++ DataBase.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.SqlClient;
+using System.IO;
+
+
+namespace FarmLandAccountingUWP
+{
+    public sealed class DataBase
+    {
+        private SqlConnection _connection;
+
+        public DataBase()
+        {
+            _connection = new SqlConnection(getDBConfig());
+        }
+
+        public void OpenConnection()
+        {
+            if (_connection.State == System.Data.ConnectionState.Closed)
+                _connection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            if (_connection.State == System.Data.ConnectionState.Open)
+                _connection.Close();
+        }
+
+        public SqlConnection GetConnection() => _connection;
+
+        private string getDBConfig()
+        {
+            var config = File.ReadAllLines("config.txt");
+            foreach (var line in config)
+            {
+                if (line.StartsWith("CONNECTION_STRING="))
+                {
+                    var connectionString = line.Substring("CONNECTION_STRING=".Length);
+                    return connectionString;
+                }
+            }
+            return null;
+        }
+    }
+}
