11458632f221bc215f9c9ebd5abd2c8ee8abeec7
VMekh8 <mekhvitaliy88@gmail.com>
2024-05-19 15:44:43 +0300

    add respository interface

---
+++ Services/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FarmLandAccountingUWP.Services
+{
+    public interface IRepository<T> where T : class
+    {
+        Task Create(T entity);
+
+        Task Update (T entity);
+
+        Task Delete();
+
+        Task<List<T>> GetAll();
+    }
+}
