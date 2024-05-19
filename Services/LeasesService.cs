d21c8246aea60119fd1cc92a51062de6979d908f
VMekh8 <mekhvitaliy88@gmail.com>
2024-05-19 16:55:07 +0300

    Make Create Method for Leases Service

--- Services/LeasesService.cs
+++ Services/LeasesService.cs
@@ -1,14 +1,42 @@
 ﻿using FarmLandAccountingUWP.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
+using Windows.UI.Popups;
 
 namespace FarmLandAccountingUWP.Services
 {
     public class LeasesService : IRepository<LeasesModel>
     {
-        public Task Create(LeasesModel entity)
+        public async Task Create(LeasesModel entity)
         {
-            throw new System.NotImplementedException();
+            DataBase db = new DataBase();
+
+            try
+            {
+
+                string query = @"INSERT INTO Leases (ContractName, StartDate, EndDate, FarmID, LandPlotID)
+                                    VALUES (@ContractName, @StartDate, @EndDate, @FarmID, @LandPlotID)";
+
+                using (SqlCommand cmd = new SqlCommand(query, db.GetConnection()))
+                {
+                    cmd.Parameters.AddWithValue("@ContractName", entity.ContractName);
+                    cmd.Parameters.AddWithValue("@StartDate", entity.StartDate);
+                    cmd.Parameters.AddWithValue("@EndDate", entity.EndDate);
+                    cmd.Parameters.AddWithValue("@FarmID", entity.FarmID);
+                    cmd.Parameters.AddWithValue("@LandPlotId", entity.LandPlotID);
+
+                    await cmd.ExecuteNonQueryAsync();
+                }
+
+            }
+            catch (Exception e)
+            {
+;               Console.WriteLine(e.Message);
+                var dialog = new MessageDialog(e.Message);
+                await dialog.ShowAsync();
+            }
         }
 
         public Task Delete()
@@ -27,3 +55,4 @@ namespace FarmLandAccountingUWP.Services
         }
     }
 }
+
